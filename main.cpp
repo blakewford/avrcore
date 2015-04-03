233837e728ea90b560917d902c5d4d237bea5dd1
blakewford <blakewford@gmail.com>
2015-04-02 20:53:44 -0500

    Add lsr instruction

--- main.cpp
+++ main.cpp
@@ -1085,6 +1085,17 @@ int32_t fetch()
                         memory[((memory[PC] & 0x1) << 4) | (memory[PC+1] >> 4)] = result;
                         PC+=2;
                         break;
+                    case 0x6: //lsr
+                        result = memory[((memory[PC] & 0x1) << 4) | (memory[PC+1] >> 4)];
+                        newStatus.C = (result & 0x1) > 0 ? SET: CLR;
+                        result = (result >> 1);
+                        newStatus.N = CLR;
+                        newStatus.V = ((newStatus.N ^ newStatus.C) > 0) ? SET: CLR;
+                        newStatus.Z = result == 0x00 ? SET: CLR;
+                        newStatus.S = ((newStatus.N ^ newStatus.V) > 0) ? SET: CLR;
+                        memory[((memory[PC] & 0x1) << 4) | (memory[PC+1] >> 4)] = result;
+                        PC+=2;
+                        break;
                     case 0x7: //ror
                         result = memory[((memory[PC] & 0x1) << 4) | (memory[PC+1] >> 4)];
                         newStatus.C = (result & 0x1) > 0 ? SET: CLR;
