5078af8ac9de91826c1b8a70be1307d75a410d2e
Blake W. Ford <blakewford@gmail.com>
2016-05-23 23:00:35 -0500

    Update Android build

--- jni/Application.mk
+++ jni/Application.mk
@@ -1,4 +1,5 @@
-#APP_ABI := all
-APP_ABI := armeabi-v7a
-APP_CPPFLAGS += -O3 -std=c++11 -DPROFILE -DATMEGA32U4
+APP_ABI := x86_64
+APP_OPTIM := debug
+APP_CPPFLAGS += -fPIE -std=c++11 -DPROFILE -DATMEGA32U4
+APP_LDFLAGS += -Wl,-pie
 APP_STL := gnustl_static
